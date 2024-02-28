4a5e633232304a5b4875b4817fa8086d51065332
James Huston <jamzhuston@gmail.com>
2024-02-28 00:08:02 -0700

    Feat: started implementing in User.cc

--- User.cc
+++ User.cc
@@ -1,3 +1,43 @@
+/*
+* @author James Huston | jamzhuston@gmail.com
+* @date 28/02/2024
+* @brief class to manage user inputs and data
+*/
 
+
+
+#include <iostream>
+#include <string>
+
+User::User() {
+  this("", "");
+}
+
+
+User::User(std::string name, std::string symbol) {
+  this->name = name;
+  this->symbol = symbol;
+}
+
+
+int User::getChoice(int[] boxes) {
+  return 0;
+}
+
+
+std::string User::getName() {
+  return "";
+}
+
+
+std::string User::getSymbol() {
+  return "";
+}
+
+
+
+std::string User::toString() {
+  return "";
+}
 
 
