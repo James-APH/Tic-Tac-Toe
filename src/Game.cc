9f0bae356831e74d73ed8d50c99d5370d2cd2036
James Huston <jamzhuston@gmail.com>
2024-03-01 14:11:49 -0700

    added forgotten semicolon at end of function

--- src/Game.cc
+++ src/Game.cc
@@ -7,9 +7,11 @@
 #include <iostream>
 #include <string>
 #include "Player.h"
+#include "Game.h"
 
+Game::Game() {
 
-Game::Game();
+}
 
 
 void Game::play(User player1, User player2) {
@@ -28,7 +30,7 @@ void Game::printScreen() {
 
 
 std::string Game::checkWinner() {
-  return ""
+  return "";
 }
 
 
