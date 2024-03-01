75ae05f1f8a48ffde6eed748e47df57505137e0e
James Huston <jamzhuston@gmail.com>
2024-02-29 20:17:01 -0700

    Feat: started on definitions for Game.cc

--- include/Game.h
+++ include/Game.h
@@ -13,7 +13,7 @@
 
 #include <iostream>
 #include <string>
-#include "User.h"
+#include "Player.h"
 
 
 
@@ -35,22 +35,18 @@ class Game {
    * @brief function to manage the game with two people
    * @param player1 one of the players
    * @param player2 one of the players
-   *
    */
   void play(User player1, User player2);
-
 
   /**
    * @brief function to manage the game with one person
    * @param player1 the player
    * @param ai the ai to defeat the player
-   *
    */
   void play(User player1, AI ai);
 
   /**
    * @brief function to print the game board to the screen
-   *
    */
   void printScreen();
 
@@ -58,17 +54,16 @@ class Game {
    * @brief function to check if a user has won,
    * @return string either the symbol of the winner,
    * or an empty string
-   *
    */
   std::string checkWinner();
 
   /**
    * @brief function to anounce the winner, display each of the scores
    *        and ask the user(s) if they want to play again.
-   *
    */
   void concludeGame();
-private:
+
+ private:
   int boxes[];
 
 }
---
+++ src/Game.cc
@@ -0,0 +1,37 @@
+/**
+ * @author James Huston | jamzhuston@gmail.com
+ * @date 29/02/2024
+ * @brief definitions for Player.h
+ */
+ 
+#include <iostream>
+#include <string>
+#include "Player.h"
+
+
+Game::Game();
+
+
+void Game::play(User player1, User player2) {
+
+}
+
+
+void Game::play(User player1, AI ai) {
+
+}
+
+
+void Game::printScreen() {
+
+}
+
+
+std::string Game::checkWinner() {
+  return ""
+}
+
+
+void Game::concludeGame() {
+
+}
